e97f7a7bfe45e63425cb50e53b2a30b4d081f36b
Georgy <shirshov.gera@bk.ru>
2024-01-28 13:19:26 +0300

    Обновлена команда для получения текущих значений валют

--- src/Command/DownloadQuotesCommand.php
+++ src/Command/DownloadQuotesCommand.php
@@ -2,48 +2,44 @@
 
 namespace App\Command;
 
+use App\Repository\CurrencyRepository;
+use App\Service\DownloadQuotesService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 #[AsCommand(
     name: 'download:quotes',
-    description: 'Add a short description for your command',
+    description: 'Получение котировок',
 )]
 class DownloadQuotesCommand extends Command
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly DownloadQuotesService $downloadQuotesService,
+        private readonly CurrencyRepository $currencyRepository,
+    ) {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        try {
+            $currencies = $this->downloadQuotesService->fetch();
+
+            $this->currencyRepository->updateOrCreateAll($currencies);
+
+            return Command::SUCCESS;
+        } catch (TransportExceptionInterface|ServerExceptionInterface|RedirectionExceptionInterface|ClientExceptionInterface $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
-        return Command::SUCCESS;
     }
 }
